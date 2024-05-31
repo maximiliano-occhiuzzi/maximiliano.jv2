a26cb31db314aeb3aa314ab0752365b63c8df32e
maximiliano occhiuzzi <omaximiliano969@gmail.com>
2024-05-31 11:56:58 -0300

    subiendo

---
+++ src/maximilianoo4tc/ejercicio61.java
@@ -0,0 +1,24 @@
+package maximilianoo4tc;
+
+import java.util.Scanner;
+
+public class ejercicio61 {
+	public static void main(String[] args) {
+		Scanner dato = new Scanner(System.in);
+		String nombre;
+		String apellido;
+		
+		
+	System.out.println("ingrese su nombre: ");
+		nombre = dato.next().toLowerCase();
+	System.out.println("ingrese su apellido: ");
+		apellido = dato.next().toUpperCase();
+		
+	System.out.println(nombre);
+	System.out.println(apellido);
+
+		
+		
+		
+	}
+}

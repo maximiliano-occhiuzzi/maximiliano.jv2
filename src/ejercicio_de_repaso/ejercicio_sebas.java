68bcd846dbb8b1218962e93caf8500f470c09ea0
maximiliano occhiuzzi <omaximiliano969@gmail.com>
2024-06-27 12:12:48 -0300

    27/06

--- src/ejercicio_de_repaso/ejercicio_sebas.java
+++ src/ejercicio_de_repaso/ejercicio_sebas.java
@@ -1,5 +1,28 @@
 package ejercicio_de_repaso;
 
-public class ejercicio_sebas {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class ejercicio_sebas {
+public static void main(String[] args) {
+	Scanner dato = new Scanner(System.in);
+	List<String> dni = new ArrayList<>();
+	System.out.println("Ingrese numeros de dni (o escriba 'fin' para terminar):");
+	String ingresodni = dato.nextLine();
+
+	while (!ingresodni.equalsIgnoreCase("fin")) {
+		dni.add(ingresodni);
+		ingresodni = dato.nextLine();
+	}
+	List<String> apellidos = new ArrayList<>();
+	System.out.println("Ingrese numeros de dni (o escriba 'fin' para terminar):");
+	String ingresoApellido = dato.nextLine();
+
+	while (!ingresoApellido.equalsIgnoreCase("fin")) {
+		dni.add(ingresoApellido);
+		ingresoApellido = dato.nextLine();
+	}
+	
+}
 }

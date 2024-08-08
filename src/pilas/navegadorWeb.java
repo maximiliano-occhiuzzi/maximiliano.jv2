048cbed7fd0f43d4ea64e795785e9989ab9b3145
maximiliano occhiuzzi <omaximiliano969@gmail.com>
2024-08-08 12:07:13 -0300

    9/8

--- src/pilas/navegadorWeb.java
+++ src/pilas/navegadorWeb.java
@@ -6,23 +6,67 @@ import java.util.Stack;
 public class navegadorWeb {
 	public static void main(String[] args) {
 		Scanner dato = new Scanner(System.in);
-		Stack<Integer> historial = new Stack<Integer>();
-		Stack<Integer> skip = new Stack<Integer>();
-		int opcion = 0;
-		boolean flag= false;
-			
-			while(opcion > 5 && opcion< 1 ){
-				System.out.println("BIENVENIDO/A A GOOGLE");
-				System.out.println("1. desea visitar una pagina nueva?");
-				System.out.println("2. volver atras");
-				System.out.println("3. avanzar");
-				System.out.println("4. mostrar historial");
-				System.out.println("5. salir");
-				opcion = dato.nextInt();
+		Stack<String> historial = new Stack<String>();
+		Stack<String> forward = new Stack<String>();
+		boolean flag = true;
+		int opcion;
+		String url = null;
+		String temporal = null;
+		System.out.println("BIENVENIDO/A A GOOGLE");
+		System.out.println("1. desea visitar una pagina nueva?");
+		System.out.println("2. volver atras");
+		System.out.println("3. avanzar");
+		System.out.println("4. mostrar historial");
+		System.out.println("5. salir");
+		opcion = dato.nextInt();
+
+		while (opcion >= 1 && opcion < 5) {
+
+			switch (opcion) {
+			case 1:
+				System.out.println("Porfavor ingrese una URL: ");
+				url = dato.next().toLowerCase().toUpperCase();
+				historial.push(url);
+				System.out.println("historia:  " + historial.peek());
+				temporal = url;
 				
-					
+				break;
+			case 2:
+				forward.push(temporal);
+					System.out.println("nueva pagina: " + forward);
+				break;
+			case 3:
+				forward.push(url);
+				System.out.println("historia:  " + historial.peek());
+				break;
+
+			case 4:
+					System.out.println("historial: " + historial );
+				break;
+
+			case 5:
+					System.out.println("saliendo...");
+				break;
+			default:
+				System.out.println("Opcion no valida.");
+				break;
 			}
-		
-		
+			System.out.println("1. desea visitar una pagina nueva?");
+			System.out.println("2. volver atras");
+			System.out.println("3. avanzar");
+			System.out.println("4. mostrar historial");
+			System.out.println("5. salir");
+			opcion = dato.nextInt();
+			historial.pop();
+			if (opcion == 5) {
+				System.out.println("gracias por visitarnos");
+
+				break;
+			}
+		}
+		System.out.println(" ");
+
+		System.out.println("------------------------------");
+		System.out.println("FIN DEL PROGRAMA");
 	}
 }
